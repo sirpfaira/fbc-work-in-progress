cf94be09387c6ebd53bcb9180a3cf000c52ef92d
sirpfaira <spfaira@yahoo.com>
2024-10-26 18:52:10 +0200

    Remove selection 2

--- app/admin/manage-countries/page.tsx
+++ app/admin/manage-countries/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
-import { useToast } from "@/components/hooks/use-toast";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { PlusCircle } from "lucide-react";
 import Columns from "./Columns";
@@ -15,10 +14,7 @@ import { Button } from "@/components/ui/button";
 import PageTitle from "@/app/components/common/PageTitle";
 
 export default function ManageCountries() {
-  const { toast } = useToast();
-  const queryClient = useQueryClient();
   const [isAddOpen, setIsAddOpen] = useState<boolean>(false);
-  const [selectedRows, setSelectedRows] = useState<string[]>([]);
   const { data, isError, error, isLoading } = useQuery({
     queryKey: ["countries"],
     queryFn: async () => {
@@ -27,36 +23,17 @@ export default function ManageCountries() {
     },
   });
 
-  const { mutate: deleteSelected, isPending } = useMutation({
-    mutationFn: async () => await axios.patch(`/api/countries`, selectedRows),
-    onSuccess: (response: any) => {
-      toast({
-        title: "Deleted successfully!",
-        description: response.data.message,
-      });
-      queryClient.invalidateQueries({ queryKey: ["countries"], exact: true });
-    },
-    onError: (response: any) => {
-      toast({
-        title: "Error!",
-        description: response?.message,
-        variant: "destructive",
-      });
-      console.log(response);
-    },
-  });
-
   if (isError) return <ErrorTile error={error.message} />;
 
   return (
-    <div className="flex flex-col space-y-5 p-5">
+    <div className="flex flex-col space-y-5 px-5">
       <div className="card flex items-center justify-between px-4 py-2">
         <PageTitle title="Manage countries" link="/admin" />
         <Button onClick={() => setIsAddOpen(true)} variant={"ghost"}>
           <PlusCircle size={26} />
         </Button>
       </div>
-      {isLoading || isPending ? (
+      {isLoading ? (
         <TableSkeleton columns={3} />
       ) : (
         <>{data && <DataTable columns={Columns} data={data} filter="name" />}</>
